0b97e8840a728c9fc010e9b5e196eb6fe0b4cd55
Saul <saulftopete2@gmail.com>
2019-01-30 10:19:15 -0800

    Initial commit

---
+++ app/src/main/java/com/example/basiclogins/Credentials.java
@@ -0,0 +1,6 @@
+package com.example.basiclogins;
+
+public class Credentials {
+    public static final String APP_ID = "EDC08CB7-94E7-E36F-FF66-098C32754E00";
+    public static final String API_KEY = "0E91BC3C-D69A-4A9B-FF11-28892594BB00";
+}
