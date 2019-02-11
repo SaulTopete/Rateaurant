dce50fcc4706c232cf4b71e8c22f0a59a0f7ec06
Saul <saulftopete2@gmail.com>
2019-02-11 09:13:58 -0800

    added button

--- app/src/main/java/com/example/basiclogins/RestaurantActivity.java
+++ app/src/main/java/com/example/basiclogins/RestaurantActivity.java
@@ -3,20 +3,50 @@ package com.example.basiclogins;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Spinner;
 
 public class RestaurantActivity extends AppCompatActivity {
 
     private EditText restaurantName;
-    private EditText restaurantwebsite;
+    private EditText restaurantWebsite;
     private EditText restaurantAddress;
     private EditText restaurantCuisine;
+    private RatingBar restaurantRating;
+    private Spinner restaurantSpinnerPrice;
+    private Button save;
+    private Button cancel;
+
+    public static final String EXTRA_RESTAURANT = "the restaurant";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+
+        wireWidgets();
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void wireWidgets() {
+        restaurantName = findViewById(R.id.edittext_activityrestaurant_name);
+        restaurantWebsite = findViewById(R.id.edittext_activityrestaurant_website);
+        restaurantAddress = findViewById(R.id.edittext_activityrestaurant_address);
+        restaurantCuisine = findViewById(R.id.edittext_activityrestaurant_cuisine);
+        restaurantRating = findViewById(R.id.ratingbar_activityrestaurant_rating);
+        restaurantSpinnerPrice = findViewById(R.id.spinner_activityrestaurant_spinny);
+        save = findViewById(R.id.button_activityrestaurant_save);
+        cancel = findViewById(R.id.button_activityrestaurant_cancel);
     }
 
     private void prefillFields() {
@@ -25,13 +55,12 @@ public class RestaurantActivity extends AppCompatActivity {
         Intent restaurantIntent = getIntent();
             Restaurant restaurant = restaurantIntent.getParcelableExtra(RestaurantListActivity.EXTRA_RESTAURANT);
         if(restaurant != null) {
-            editTextName.setText(restaurant.getName());
-            editTextWebsite.setText(restaurant.getWebsiteLink());
-            editTextAddress.setText(restaurant.getAddress());
-            editTextCuisine.setText(restaurant.getCuisine());
-            ratingBarRating.setRating((float) restaurant.getRating());
-            spinnerPrice.setSelection(restaurant.getPrice() - 1); //offset for 0 indexing
-
+            restaurantName.setText(restaurant.getName());
+            restaurantWebsite.setText(restaurant.getWebsiteLink());
+            restaurantAddress.setText(restaurant.getAddress());
+            restaurantCuisine.setText(restaurant.getCuisine());
+            restaurantRating.setRating((float) restaurant.getRating());
+            restaurantSpinnerPrice.setSelection(restaurant.getPrice() - 1); //offset for 0 indexing
         }
     }
 }
